f43bb00226b4e1a3c1922b7e55aabc9cbdc65e11
川鍋僚太 <m18048@g.merto-cit.ac.jp>
2024-01-08 18:46:37 +0900

    他のリリースを表示

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,59 @@ import { useState } from 'react';
 
 export default function App() {
   const [result, setResult] = useState("");
+  const [releases, setReleases] = useState("");
 
-  const get = (e) => {
-    e.preventDefault();
-    let catno = document.getElementById("catno_form");
 
-    console.log(catno.value);
+  function sleep(waitMsec) {
+    var startMsec = new Date();
+  
+    // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+    while (new Date() - startMsec < waitMsec);
+  }
+  
+
+  function get(){
+    let keyWord = document.getElementById("keyWord_form").value;
+
+    console.log(keyWord);
     const client = new Discojs({
       userToken: "TjGHBHwkCBHRcOeoyJKoVaVFpOwtkGXwffmXaLJM",
     });
 
     client
-      .searchRelease(catno.value, "catno")
-      .then(data => data.results[0] ? fetch(data.results[0].resource_url):setResult("404"))
-      .then(res => res.json())
-      .then(json => setResult(json));
+      .searchRelease(keyWord)
+      .then(function(data){
+        console.log(data);
+        (data.results ? fetch(data.results[0].resource_url):setResult("404"))
+        .then(res => res.json())
+        .then(function(json){
+          client.searchRelease(json.artists[0].name, "artist")
+          .then(data => setReleases(data.results));
+          setResult(json);
+          console.log(json);
+        });
 
-    console.log(result);
-
-    if(result === ""){
-      console.log("NOT FOUND!");
-    }
-
+        console.log(releases);
+        if(result === ""){
+          console.log("NOT FOUND!");
+        }
+      });
   }
+
   return (
     <ChakraProvider theme={theme}>
       <Heading m={2}>Vinyl Finder</Heading>
-      レコード盤やCDなどの型番からアルバム情報などを検索できます
+      アルバム情報を検索できます
       <FormControl className="searchForm" m={2} borderWidth='1.5px'  borderRadius='lg'  borderColor='black' width='auto'>
-        <label>型番を入力してください 例:SVWC-70613<br></br>
-          <Input type="text" id="catno_form" width="auto"/>
+        <label>アルバム名もしくは型番を入力してください 例:SVWC-70613<br></br>
+          <Input type="text" id="keyWord_form" width="auto"/>
           <Button onClick={get}>検索</Button>
         </label>
       </FormControl>
       <Box className="resultField" m={2} borderWidth='1.5px'  borderRadius='lg'  borderColor='black' minH="660">
         <Text as='b'>検索結果</Text><br></br>
         {result === "404" && (<Text color='tomato' as='b'>NOT FOUND</Text>)}
-        {(result !== "404" && result ) &&
+        {(result !== "404" && result && releases) &&
           (
             <Flex>
               <Box className="info" m={2} borderWidth='1.5px'  borderRadius='lg' borderColor='black' height='auto'>
@@ -66,6 +82,15 @@ export default function App() {
                 </Flex>
                 <Link href={result.uri}><strong>Discogsでのページを開く</strong></Link><br></br>
               </Box>
+              <Box className="releases" m={2} borderWidth='1.5px'  borderRadius='lg'  borderColor='black' width="40%">
+                  <Text as='b'>他のリリース</Text>
+                  <ul>
+                    {releases.map( r =>
+                      // <ul key={t.uri}>・{t.title}</ul>
+                      <ul key={r.uri}><Link href={"https://www.discogs.com/ja/release"+r.uri}>{r.title}</Link></ul>
+                    )}
+                  </ul>
+                </Box>
             </Flex>
           )
         }
